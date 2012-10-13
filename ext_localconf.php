309d4e524ddfef3eaae02f1a422010140db28fdd
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-13 17:33:25 +0000

    1.1.0 ### Publishing code in the SVN repository
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/postfix/trunk@67034 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,12 +5,12 @@ if (!defined ('TYPO3_MODE')) {
 }
 
   // Get the extensions's configuration
-$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['scheduler']);
+$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['postfix']);
 
   // If sample tasks should be shown,
   // register information for the test and sleep tasks
 if (!empty($extConf['showSampleTasks'])) {
-  $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TestTask'] = array(
+  $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_postfix_TestTask'] = array(
     'extension'        => $_EXTKEY,
     'title'            => 'LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:testTask.name',
     'description'      => 'LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:testTask.description',
