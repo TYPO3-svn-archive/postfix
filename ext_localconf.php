41ab38488a29943e62d0a8eeb5a4024b0e8f8931
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-13 17:18:07 +0000

    1.1.0 ### Publishing code in the SVN repository
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/postfix/trunk@67028 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,14 +4,17 @@ if (!defined ('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-    // If sample tasks should be shown,
-    // register information for the test and sleep tasks
+  // Get the extensions's configuration
+$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['scheduler']);
+
+  // If sample tasks should be shown,
+  // register information for the test and sleep tasks
 if (!empty($extConf['showSampleTasks'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TestTask'] = array(
-        'extension'        => $_EXTKEY,
-        'title'            => 'LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:testTask.name',
-        'description'      => 'LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:testTask.description',
-        'additionalFields' => 'tx_scheduler_TestTask_AdditionalFieldProvider'
-    );
+  $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TestTask'] = array(
+    'extension'        => $_EXTKEY,
+    'title'            => 'LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:testTask.name',
+    'description'      => 'LLL:EXT:' . $_EXTKEY . '/locallang_db.xml:testTask.description',
+    'additionalFields' => 'tx_scheduler_TestTask_AdditionalFieldProvider'
+  );
 }
 ?>
