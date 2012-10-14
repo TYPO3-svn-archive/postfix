a63bddb62d96981003f27bd01963dca8be46b721
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-14 21:24:03 +0000

    1.1.0 +++ #41953: Quota
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/postfix/trunk@67077 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/scheduler/class.tx_postfix_quotatask.php
+++ lib/scheduler/class.tx_postfix_quotatask.php
@@ -76,6 +76,13 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
     var $drsModeInfo;
     
   /**
+    * DRS mode: display prompt in performance case 
+    *
+    * @var boolean $drsModePerformance
+    */
+    var $drsModePerformance;
+    
+  /**
     * DRS mode: display prompt in quotaTask case 
     *
     * @var boolean $drsModeQuotaTask
@@ -116,21 +123,27 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
   {
     $success = true;
     
-      // Get the extensions's configuration
+      // Get the extension configuration by the extension manager
     $this->extConf = unserialize( $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['postfix'] );
 
-      // Init the DRS - Development Repöorting System
+      // Init the DRS - Development Reporting System
     $this->initDRS( );
     
+    $debugTrailLevel = 1;
+    $this->timeTracking_log( $debugTrailLevel, 'START' );
+
       // Get the data of each postfix account from the database
     $this->accountsData = $this->initAccountsData( );
-
     
-    if( ! $this->sendMailToAdmin( ) )
+      // Loop all accounts;
+    if( ! $this->executeLoopAllAccounts( ) )
     {
       $success = false;
     }
-
+      // Loop all accounts;
+    
+      // RETURN : the success
+    $this->timeTracking_log( $debugTrailLevel, 'END' );
     return $success;
   }
 
@@ -143,21 +156,27 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
    **********************************************/
   
   /**
-    * initAccountsData( ) : 
+    * initAccountsData( ) : Get the data of each postfix account from the database.
     *
-    * @return string Information to display
+    * @return   array   $rows
     * @version       1.1.0
     * @since         1.1.0
     */
   private function initAccountsData( )
   {
       // Query
-    $select_fields  = "uid, username, CONCAT( tx_postfix_homedir, '/', tx_postfix_maildir ) AS 'pathToFolder', tx_postfix_quota";
+    $select_fields  = "
+                        uid, 
+                        username, 
+                        first_name, 
+                        last_name, 
+                        CONCAT( tx_postfix_homedir, '/', tx_postfix_maildir ) AS 'pathToFolder', 
+                        tx_postfix_quota
+                      ";
     $from_table     = "fe_users";
-    $where_clause   = "tx_postfix_webmail = 1";
-    $where_clause   = null;
+    $where_clause   = "(tx_postfix_homedir IS NOT NULL OR tx_postfix_maildir IS NOT NULL";
     $groupBy        = null;
-    $orderBy        = "uid";
+    $orderBy        = "pathToFolder";
     $limit          = null;
       // Query
 
@@ -174,7 +193,7 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
                   $limit
                 );
       $prompt = $query;
-      t3lib_div::devlog( '[INFO/SQL] ' . $prompt, $this->extKey, 0 );
+      t3lib_div::devlog( '[tx_postfix_QuotaTask] ' . $prompt, $this->extKey, 0 );
     }
       // DRS
 
@@ -198,13 +217,17 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
     }
       // Get array with TYPO3Groups
     
-//    $prompt = $str_records;
-//    t3lib_div::devlog( '[INFO/SQL] ' . $prompt, $this->extKey, 0 );
+      // DRS
+    if( $this->drsModeSql )
+    {
+      $prompt = '#' . count( $rows ) . ' accounts found.';
+      t3lib_div::devlog( '[tx_postfix_QuotaTask] ' . $prompt, $this->extKey, 0 );
+    }
     return $rows;
   }
 
   /**
-    * initDRS( )  : 
+    * initDRS( )  : Init the DRS - Development Reporting System
     *               
     * @return void
     * @version       1.1.0
@@ -229,13 +252,15 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
     switch( $this->extConf['drs_mode'] )
     {
       case( 'Enabled (for debugging only!)' ):
-        $this->drsModeQuotaTask = true;
-        $this->drsModeSql       = true;
+        $this->drsModePerformance = true;
+        $this->drsModeQuotaTask   = true;
+        $this->drsModeSql         = true;
         break;
       default:
           // :TODO: Error msg per email to admin
-        $this->drsModeQuotaTask = true;
-        $this->drsModeSql       = true;
+        $this->drsModePerformance = true;
+        $this->drsModeQuotaTask   = true;
+        $this->drsModeSql         = true;
         $prompt = 'DRS mode isn\'t defined.';
         t3lib_div::devlog( '[tx_postfix_QuotaTask] ' . $prompt, $this->extKey, 3 );
         break;
@@ -243,14 +268,14 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
   }
 
   /**
-    * sendMailToAdmin( )  : Function executed from the Scheduler.
+    * executeLoopAllAccounts( )  : Function executed from the Scheduler.
     *               * Sends an email
     *
     * @return boolean
     * @version       1.1.0
     * @since         1.1.0
     */
-  private function sendMailToAdmin( )
+  private function executeLoopAllAccounts( )
   {
     $success = false;
     
@@ -347,7 +372,169 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
 
     return $success;
   }
+    /***********************************************
+   *
+   * DRS - Development Reporting System
+   *
+   **********************************************/
+
+
+
+/**
+ * drs_debugTrail( ): Returns class, method and line of the call of this method.
+ *                    The calling method is a debug method - if it is called by another
+ *                    method, please set the level in the calling method to 2.
+ *
+ * $param   integer   $level      : integer
+ *
+ * @param    [type]        $$level: ...
+ * @return    array        $arr_return : with elements class, method, line and prompt
+ * @version 3.9.9
+ * @since   3.9.9
+ */
+  public function drs_debugTrail( $level = 1 )
+  {
+    $arr_return = null; 
+    
+      // Get the debug trail
+    $debugTrail_str = t3lib_utility_Debug::debugTrail( );
+
+      // Get debug trail elements
+    $debugTrail_arr = explode( '//', $debugTrail_str );
+
+      // Get class, method
+    $classMethodLine = $debugTrail_arr[ count( $debugTrail_arr) - ( $level + 2 )];
+    list( $classMethod ) = explode ( '#', $classMethodLine );
+    list($class, $method ) = explode( '->', $classMethod );
+      // Get class, method
+
+      // Get line
+    $classMethodLine = $debugTrail_arr[ count( $debugTrail_arr) - ( $level + 1 )];
+    list( $dummy, $line ) = explode ( '#', $classMethodLine );
+    unset( $dummy );
+      // Get line
+
+      // RETURN content
+    $arr_return['class']  = trim( $class );
+    $arr_return['method'] = trim( $method );
+    $arr_return['line']   = trim( $line );
+    $arr_return['prompt'] = $arr_return['class'] . '::' . $arr_return['method'] . ' (' . $arr_return['line'] . ')';
+
+    return $arr_return;
+      // RETURN content
+  }
+
+
+
   
+  
+  /***********************************************
+   *
+   * Time tracking
+   *
+   **********************************************/
+
+  /**
+    * timeTracking_init( ):  Init the timetracking object. Set the global $startTime.
+    *
+    * @return    void
+    * @version       1.1.0
+    * @since         1.1.0
+    */
+  private function timeTracking_init( )
+  {
+      // Init the timetracking object
+    require_once( PATH_t3lib . 'class.t3lib_timetrack.php' );
+    $this->TT = new t3lib_timeTrack;
+    $this->TT->start( );
+      // Init the timetracking object
+
+      // Set the global $startTime.
+    $this->tt_startTime = $this->TT->mtime();
+  }
+
+  /**
+    * timeTracking_log( ): Prompts a message in devLog with current run time in miliseconds
+    *
+    * @param    integer        $debugTrailLevel  : level for the debug trail
+    * @param    string        $line             : current line in calling method
+    * @param    string        $prompt           : The prompt for devlog.
+    * @return    void
+    * @version       1.1.0
+    * @since         1.1.0
+    */
+  private function timeTracking_log( $debugTrailLevel, $prompt )
+  {
+      // RETURN: DRS shouldn't report performance prompts
+    if( ! $this->drsModePerformance )
+    {
+      return;
+    }
+      // RETURN: DRS shouldn't report performance prompts
+
+      // Get the current time
+    $endTime = $this->TT->mtime( );
+
+    $debugTrail = $this->drs_debugTrail( $debugTrailLevel );
+
+    // Prompt the current time
+    $mSec   = sprintf("%05d", ( $endTime - $this->tt_startTime ) );
+    $prompt = $mSec . ' ms ### ' . 
+              $debugTrail['prompt'] . ': ' . $prompt;
+    t3lib_div::devLog( $prompt, $this->extKey, 0 );
+
+    $timeOfPrevProcess = $endTime - $this->tt_prevEndTime;
+    
+    switch( true )
+    {
+      case( $timeOfPrevProcess >= 10000 ):
+        $this->tt_prevPrompt = 3;
+        $prompt = 'Previous process needs more than 10 sec (' . $timeOfPrevProcess / 1000 . ' sec)';
+        t3lib_div::devLog('[WARN/PERFORMANCE] ' . $prompt, $this->extKey, 3 );
+        break;
+      case( $timeOfPrevProcess >= 250 ):
+        $this->tt_prevPrompt = 2;
+        $prompt = 'Previous process needs more than 0.25 sec (' . $timeOfPrevProcess / 1000 . ' sec)';
+        t3lib_div::devLog('[WARN/PERFORMANCE] ' . $prompt, $this->extKey, 2 );
+        break;
+      default:
+        $this->tt_prevPrompt = 0;
+        // Do nothing
+    }
+    $this->tt_prevEndTime = $endTime;
+  }
+
+  /**
+    * timeTracking_prompt( ):  Method checks, wether previous prompt was a
+    *                          warning or an error. If yes the given prompt will loged by devLog
+    *
+    * @param    integer        $debugTrailLevel  : level for the debug trail
+    * @param    string        $prompt: The prompt for devlog.
+    * @return    void
+    * @version       1.1.0
+    * @since         1.1.0
+    */
+  private function timeTracking_prompt( $debugTrailLevel, $prompt )
+  {
+    $debugTrail = $this->drs_debugTrail( $debugTrailLevel );
+
+    switch( true )
+    {
+      case( $this->tt_prevPrompt == 3 ):
+        $prompt_02 = 'ERROR';
+        break;
+      case( $this->tt_prevPrompt == 2 ):
+        $prompt_02 = 'WARN';
+        break;
+      default:
+          // Do nothing
+        return;
+    }
+
+    $prompt = 'Details about previous process: ' . $prompt . ' (' . $debugTrail['prompt'] . ')';
+    t3lib_div::devLog('[INFO/PERFORMANCE] ' . $prompt, $this->extKey, $this->tt_prevPrompt );
+  }
+
   
   
   
