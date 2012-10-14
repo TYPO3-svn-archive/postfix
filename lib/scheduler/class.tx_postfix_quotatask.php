d4993fd207627c3d7c51ecc44217ce156c1d547c
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-14 20:29:33 +0000

    1.1.0 +++ #41953: Quota
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/postfix/trunk@67073 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/scheduler/class.tx_postfix_quotatask.php
+++ lib/scheduler/class.tx_postfix_quotatask.php
@@ -34,13 +34,75 @@
 class tx_postfix_QuotaTask extends tx_scheduler_Task {
 
   /**
+    * Extension key
+    *
+    * @var string $extKey
+    */
+    var $extKey = 'postfix';
+    
+  /**
+    * Extension configuration by the extension manager
+    *
+    * @var array $extConf
+    */
+    var $extConf;
+
+  /**
+    * DRS mode: display prompt in every case
+    *
+    * @var boolean $drsModeAll
+    */
+    var $drsModeAll;
+    
+  /**
+    * DRS mode: display prompt in error case only 
+    *
+    * @var boolean $drsModeError
+    */
+    var $drsModeError;
+    
+  /**
+    * DRS mode: display prompt in warning case only
+    *
+    * @var boolean $drsModeWarn
+    */
+    var $drsModeWarn;
+    
+  /**
+    * DRS mode: display prompt in info case only
+    *
+    * @var boolean $drsModeInfo
+    */
+    var $drsModeInfo;
+    
+  /**
+    * DRS mode: display prompt in quotaTask case 
+    *
+    * @var boolean $drsModeQuotaTask
+    */
+    var $drsModeQuotaTask;
+    
+  /**
+    * DRS mode: display prompt in sql case
+    *
+    * @var boolean $drsModeSql
+    */
+    var $drsModeSql;
+    
+  /**
     * An email address to be used during the process
     *
-    * @var string $email
+    * @var string $postfix_postfixAdminEmail
     */
     var $postfix_postfixAdminEmail;
     
-    var $extKey = 'postfix';
+  /**
+    * All Postfix accounts returned from database
+    *
+    * @var array $accountsData
+    */
+    var $accountsData;
+    
 
   /**
     * execute( )  : Function executed from the Scheduler.
@@ -54,12 +116,130 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
   {
     $success = true;
     
+      // Get the extensions's configuration
+    $this->extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['postfix']);
+
+      // Init the DRS - Development Repöorting System
+    $this->initDRS( );
+    
+      // Get the data of each postfix account from the database
+    $this->accountsData = $this->initAccountsData( );
+
+    
     if( ! $this->sendMailToAdmin( ) )
     {
       $success = false;
     }
 
     return $success;
+  }
+
+  
+  
+  /***********************************************
+   *
+   * Initials
+   *
+   **********************************************/
+  
+  /**
+    * initAccountsData( ) : 
+    *
+    * @return string Information to display
+    * @version       1.1.0
+    * @since         1.1.0
+    */
+  private function initAccountsData( )
+  {
+      // Query
+    $select_fields  = "uid, username, CONCAT( tx_postfix_homedir, '/', tx_postfix_maildir ) AS 'pathToFolder', tx_postfix_quota";
+    $from_table     = "fe_users";
+    $where_clause   = "tx_postfix_webmail = 1";
+    $where_clause   = null;
+    $groupBy        = null;
+    $orderBy        = "uid";
+    $limit          = null;
+      // Query
+
+      // DRS
+    if( $this->drsModeSql )
+    {
+      $query  = $GLOBALS['TYPO3_DB']->SELECTquery
+                (
+                  $select_fields,
+                  $from_table,
+                  $where_clause,
+                  $groupBy,
+                  $orderBy,
+                  $limit
+                );
+      $prompt = $query;
+      t3lib_div::devlog( '[INFO/SQL] ' . $prompt, $this->extKey, 0 );
+    }
+      // DRS
+
+      // SELECT
+    $res =  $GLOBALS['TYPO3_DB']->exec_SELECTquery
+            (
+              $select_fields,
+              $from_table,
+              $where_clause,
+              $groupBy,
+              $orderBy,
+              $limit
+            );
+      // SELECT
+
+      // Get array with TYPO3Groups
+    $rows = array( );
+    while( $row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc( $res ) )
+    {
+      $rows[ ]  = ( array ) $row;
+    }
+      // Get array with TYPO3Groups
+    
+//    $prompt = $str_records;
+//    t3lib_div::devlog( '[INFO/SQL] ' . $prompt, $this->extKey, 0 );
+    return $rows;
+  }
+
+  /**
+    * initDRS( )  : 
+    *               
+    * @return void
+    * @version       1.1.0
+    * @since         1.1.0
+    */
+  private function initDRS( )
+  {
+
+    if( $this->extConf['drs_mode'] == 'Disabled' )
+    {
+      return;
+    }
+    
+    $this->drsModeAll   = true;
+    $this->drsModeError = true;
+    $this->drsModeWarn  = true;
+    $this->drsModeInfo  = true;
+
+    $prompt = 'DRS - Development Reporting System: ' . $this->extConf['drs_mode'];
+    t3lib_div::devlog( '[tx_postfix_QuotaTask] ', $this->extKey, 0 );
+
+    switch( $this->extConf['drs_mode'] )
+    {
+      case( 'Enabled (for debugging only!)' ):
+        $this->drsModeQuotaTask = true;
+        $this->drsModeSql       = true;
+        break;
+      default:
+          // :TODO: Error msg per email to admin
+        $this->drsModeQuotaTask = true;
+        $this->drsModeSql       = true;
+        $prompt = 'DRS mode isn\'t defined.';
+        t3lib_div::devlog( '[tx_postfix_QuotaTask] ', $this->extKey, 3 );
+        break;
+    }
   }
 
   /**
@@ -77,9 +257,13 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
       // RETURN : no email address is given
     if ( empty( $this->postfix_postfixAdminEmail ) ) 
     {
-        // No email defined, just log the task
-      $prompt = 'email address is missing for the Postfix admin.';
-      t3lib_div::devLog( '[tx_postfix_QuotaTask]: ' . $prompt, $this->extKey, 2 );
+        // DRS
+      if( $this->drsModeQuotaTask )
+      {
+        $prompt = 'email address is missing for the Postfix admin.';
+        t3lib_div::devLog( '[tx_postfix_QuotaTask]: ' . $prompt, $this->extKey, 2 );
+      }
+        // DRS
       return $success;
     }
       // RETURN : no email address is given
@@ -100,6 +284,8 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
       // Get execution information
     $exec = $this->getExecution();
 
+    $strAccountData = var_export( $this->accountsData, true );
+
     $start    = $exec->getStart();
     $end      = $exec->getEnd();
     $interval = $exec->getInterval();
@@ -119,8 +305,7 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
       'multiple: ' . ($multiple ? 'yes' : 'no') . PHP_EOL .
       'cronCmd: ' . ($cronCmd ? $cronCmd : 'not used') . PHP_EOL .
       PHP_EOL .
-      $this->sql( ) . PHP_EOL .
-      PHP_EOL .
+      $strAccountData . PHP_EOL .
       'XXX'
       ;
 
@@ -143,84 +328,34 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
       throw new t3lib_exception( $e->getMessage( ) );
     }
 
-    switch( $success )
+      // DRS
+    if( $this->drsModeQuotaTask )
     {
-      case( false ):
-        $prompt = 'Undefined error. Test email couldn\'t sent to "' . $this->postfix_postfixAdminEmail . '"';
-        t3lib_div::devLog( '[tx_postfix_QuotaTask]: ' . $prompt, $this->extKey, 3 );
-        break;
-      case( true ):
-      default:
-        $prompt = 'Test email is sent to "' . $this->postfix_postfixAdminEmail . '"';
-        t3lib_div::devLog( '[tx_postfix_QuotaTask]: ' . $prompt, $this->extKey, -1 );
-        break;
+      switch( $success )
+      {
+        case( false ):
+          $prompt = 'Undefined error. Test email couldn\'t sent to "' . $this->postfix_postfixAdminEmail . '"';
+          t3lib_div::devLog( '[tx_postfix_QuotaTask]: ' . $prompt, $this->extKey, 3 );
+          break;
+        case( true ):
+        default:
+          $prompt = 'Test email is sent to "' . $this->postfix_postfixAdminEmail . '"';
+          t3lib_div::devLog( '[tx_postfix_QuotaTask]: ' . $prompt, $this->extKey, -1 );
+          break;
+      }
     }
 
     return $success;
   }
-
-  /**
-    * sql( ) : 
-    *
-    * @return string Information to display
-    * @version       1.1.0
-    * @since         1.1.0
-    */
-  private function sql( )
-  {
-      // Query
-    $select_fields  = "uid, username, CONCAT( tx_postfix_homedir, tx_postfix_maildir ) AS 'pathToFolder', tx_postfix_quota";
-    $from_table     = "fe_users";
-    $where_clause   = "tx_postfix_webmail = 1";
-    $where_clause   = null;
-    $groupBy        = null;
-    $orderBy        = "uid";
-    $limit          = null;
-      // Query
-
-      // DRS prompt
-//    if ( $this->b_drs_sql )
-//    {
-      $query  = $GLOBALS['TYPO3_DB']->SELECTquery
-                (
-                  $select_fields,
-                  $from_table,
-                  $where_clause,
-                  $groupBy,
-                  $orderBy,
-                  $limit
-                );
-      $prompt = $query;
-      t3lib_div::devlog( '[INFO/SQL] ' . $prompt, $this->extKey, 0 );
-//    }
-      // DRS prompt
-
-      // SELECT
-    $res =  $GLOBALS['TYPO3_DB']->exec_SELECTquery
-            (
-              $select_fields,
-              $from_table,
-              $where_clause,
-              $groupBy,
-              $orderBy,
-              $limit
-            );
-      // SELECT
-
-      // Get array with TYPO3Groups
-    $arr_records = array( );
-    while( $row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc( $res ) )
-    {
-      $arr_records[ ] = implode( ',', ( array ) $row );
-    }
-      // Get array with TYPO3Groups
-    
-    $str_records = implode( PHP_EOL, ( array ) $arr_records );
-
-    $prompt = $str_records;
-    t3lib_div::devlog( '[INFO/SQL] ' . $prompt, $this->extKey, 0 );
-    return $str_records;
-  }
+  
+  
+  
+  
+  /***********************************************
+   *
+   * Scheduler Form
+   *
+   **********************************************/
 
   /**
     * getAdditionalInformation( ) : This method returns the destination mail address as additional information
@@ -230,7 +365,7 @@ class tx_postfix_QuotaTask extends tx_scheduler_Task {
     * @since         1.1.0
     */
   public function getAdditionalInformation( )
-  {
+  {    
     return $GLOBALS['LANG']->sL('LLL:EXT:postfix/lib/scheduler/locallang.xml:label.postfixAdminEmail') . ': ' . $this->postfix_postfixAdminEmail;
   }
 }
